f016fb73ad4ba765ed87c0660626dcf05e41ed73
veronmolosh <nikabu17@gmail.com>
2021-03-10 14:07:54 +0300

    feat: add solution in index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
+exports.min = function min(array) {
+    return array && array.length ? Math.min(...array) : 0;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    return array && array.length ? Math.max(...array) : 0;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    } else {
+        let sum = array.reduce((a, b) => a + b);
+        return (sum / array.length);
+    }
 }
